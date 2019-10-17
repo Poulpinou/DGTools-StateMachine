6afce7e92a5c2c3e2fc92c2ffc3ed7c9eadac132
Donovan <donovan.persent@gmail.com>
2019-10-17 12:56:23 +0200

    Fix gitignore + Issues #4, #5, #6

--- Runtime/Scripts/StateMachine.cs
+++ Runtime/Scripts/StateMachine.cs
@@ -1,45 +1,66 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System;
+using UnityEngine.Events;
 
 namespace DGTools.StateMachine
 {
     public class StateMachine : MonoBehaviour
     {
-        //VARIABLES
-        protected State _currentState;
-        protected bool _inTransition = false;
+        #region Private Variables
+        protected State currentState;
+        #endregion
 
-        //PROPERTIES
+        #region Events
+        [Serializable] public class StateMachineEvent : UnityEvent<StateMachine> { }
+        [Header("Events")]
+        public StateMachineEvent OnStateWillChange = new StateMachineEvent();
+        public StateMachineEvent OnStateChanged = new StateMachineEvent();
+        public StateMachineEvent OnStateUnlocked = new StateMachineEvent();
+        #endregion
+
+        #region Properties
         /// <summary>
         /// Returns the current state of the state machine
         /// </summary>
-        public virtual State currentState
+        public virtual State CurrentState
         {
-            get { return _currentState; }
+            get { return currentState; }
             set { Transition(value); }
         }
 
         /// <summary>
+        /// Override this to set default <see cref="State"/>, the <see cref="StateMachine"/> will enter this <see cref="State"/> on Awake
+        /// </summary>
+        public virtual Type DefaultState => null;
+
+        /// <summary>
+        /// Override this to set a <see cref="Type"/> contraint to <see cref="State"/>. A <see cref="State"/> should inherit from this type to be handled by this <see cref="StateMachine"/>
+        /// </summary>
+        public virtual Type StateTypeConstraint => typeof(State);
+
+        /// <summary>
         /// StateMachine is locked if current state is locked, call <see cref="UnlockCurrentState()"/> to unlock it 
         /// </summary>
-        public virtual bool isLocked
+        public virtual bool IsLocked
         {
             get
             {
-                if (currentState != null && currentState.isLocked)
+                if (CurrentState != null && CurrentState.IsLocked)
                 {
                     Debug.Log(string.Format(
                         "State {0} is locked, unlock it to change state",
-                        currentState.GetType()
+                        CurrentState.GetType()
                     ));
                     return true;
                 }
                 return false;
             }
-        }       
+        }
 
-        //METHODS
+        public bool InTransition { get; protected set; } = false;
+        #endregion
+
+        #region Public Methods
         /// <summary>
         /// Returns a State of type <typeparamref name="Tstate"/> attached to the StateMachine, it adds it to the StateMachine if not found
         /// </summary>
@@ -47,6 +68,7 @@ namespace DGTools.StateMachine
         /// <returns>The State of type <typeparamref name="Tstate"/> attached to the machine</returns>
         public virtual Tstate GetState<Tstate>() where Tstate : State
         {
+            if (!IsValidStateType(typeof(Tstate))) return null;
             Tstate target = GetComponent<Tstate>();
             if (target == null)
                 target = gameObject.AddComponent<Tstate>();
@@ -63,7 +85,7 @@ namespace DGTools.StateMachine
         public virtual Tstate GetState<Tstate, Tparam>(Tparam param) where Tstate : StateWithParams<Tparam>
         {
             Tstate target = GetState<Tstate>();
-            target.param = param;
+            target.Params = param;
             return target;
         }
 
@@ -74,20 +96,14 @@ namespace DGTools.StateMachine
         /// <returns>The State of give Type attached to the machine</returns>
         public virtual State GetState(Type type)
         {
-            if (type.IsSubclassOf(typeof(State)))
+            if (!IsValidStateType(type)) return null;
+            State target = (State)GetComponent(type);
+            if (target == null)
             {
-                State target = (State)GetComponent(type);
-                if (target == null)
-                {
-                    gameObject.AddComponent(type);
-                    target = (State)GetComponent(type);
-                }
-                return target;
+                gameObject.AddComponent(type);
+                target = (State)GetComponent(type);
             }
-            else
-            {
-                throw new Exception(type.ToString() + " does not inherit from state class");
-            }
+            return target;
         }
         
         /// <summary>
@@ -95,8 +111,8 @@ namespace DGTools.StateMachine
         /// </summary>
         public virtual void ClearState()
         {
-            DestroyImmediate(_currentState, true);
-            _currentState = null;
+            DestroyImmediate(CurrentState, true);
+            CurrentState = null;
         }
 
         /// <summary>
@@ -105,8 +121,8 @@ namespace DGTools.StateMachine
         /// <typeparam name="Tstate">The type of the State</typeparam>
         public virtual void ChangeState<Tstate>() where Tstate : State
         {
-            if (isLocked) return;
-            currentState = GetState<Tstate>();
+            if (IsLocked) return;
+            CurrentState = GetState<Tstate>();
         }
 
         /// <summary>
@@ -117,8 +133,8 @@ namespace DGTools.StateMachine
         /// <param name="param">Value of state's param</param>
         public virtual void ChangeState<Tstate, Tparam>(Tparam param) where Tstate : StateWithParams<Tparam>
         {
-            if (isLocked) return;
-            currentState = GetState<Tstate, Tparam>(param);
+            if (IsLocked) return;
+            CurrentState = GetState<Tstate, Tparam>(param);
         }
 
         /// <summary>
@@ -127,10 +143,10 @@ namespace DGTools.StateMachine
         /// <param name="type">Type of the State</param>
         public virtual void ChangeState(Type type)
         {
-            if (isLocked) return;
+            if (IsLocked) return;
             try
             {
-                currentState = GetState(type);
+                CurrentState = GetState(type);
             }
             catch (Exception e)
             {
@@ -140,35 +156,73 @@ namespace DGTools.StateMachine
         }
 
         /// <summary>
-        /// Performs the transition between current State and new State
-        /// </summary>
-        /// <param name="value">The new State</param>
-        protected virtual void Transition(State value)
-        {
-            if (_currentState == value || _inTransition)
-                return;
-            _inTransition = true;
-            if (_currentState != null)
-                _currentState.Exit();
-            _currentState = value;
-            if (_currentState != null)
-                _currentState.Enter();
-            _inTransition = false;
-        }
-
-        /// <summary>
         /// Locks the current State
         /// </summary>
-        public void LockCurrentState() {
-            currentState.isLocked = true;
+        public void LockCurrentState(UnityAction<StateMachine> onUnlock = null)
+        {
+            CurrentState.IsLocked = true;
+
+            if(onUnlock != null)
+            {
+                OnStateUnlocked.AddListener(onUnlock);
+            }
         }
 
         /// <summary>
         /// Unlocks the current State
         /// </summary>
-        public void UnlockCurrentState()
+        public void UnlockCurrentState(bool removeUnlockActions = true)
         {
-            currentState.isLocked = false;
+            CurrentState.IsLocked = false;
+            OnStateUnlocked.Invoke(this);
+
+            if (removeUnlockActions)
+                OnStateUnlocked.RemoveAllListeners();
         }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Performs the transition between current State and new State
+        /// </summary>
+        /// <param name="value">The new State</param>
+        protected virtual void Transition(State value)
+        {
+            if (currentState == value || InTransition)
+                return;
+            InTransition = true;
+            OnStateWillChange.Invoke(this);
+            if (currentState != null)
+                currentState.Exit();
+            currentState = value;
+            if (currentState != null)
+                currentState.Enter();
+            else
+                ClearState();
+            InTransition = false;
+            OnStateChanged.Invoke(this);
+        }
+        #endregion
+
+        #region Runtime Methods
+        private void Awake()
+        {
+            if(DefaultState != null)
+            {
+                ChangeState(DefaultState);
+            }
+        }
+
+        protected virtual bool IsValidStateType(Type type)
+        {
+            if (!type.IsSubclassOf(typeof(State)))
+                throw new Exception(string.Format("{0} should inherit from State to be used in a StateMachine", type.Name));
+
+            if (!type.IsSubclassOf(StateTypeConstraint))
+                throw new Exception(string.Format("{0} should inherit from {1} to be used in this StateMachine", type.Name, StateTypeConstraint.Name));
+
+            return true;
+        }
+        #endregion
     }
 }
